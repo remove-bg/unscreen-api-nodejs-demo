c6c3d0dc60f0216c80c0b212e6d72fb32477a374
Riccardo Porrini <r.porrini@be-tse.it>
2021-07-26 17:47:57 +0200

    documentation and cleanup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 const app = express();
 const port = 3000;
 const upload = require('./server/upload');
@@ -16,13 +15,14 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 
 // the endpoint for our file upload form
-app.post('/upload', upload(UNSCREEN_API_VIDEOS_URL))
+app.post('/upload', upload(UNSCREEN_API_VIDEOS_URL));
 
 // fetches a list of all of your videos from the unscreen API
 app.get('/videos', videos(UNSCREEN_API_VIDEOS_URL));
 
+// webhook for asyncronous communication from the unscreen API
 app.post('/webhook', webhook);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
